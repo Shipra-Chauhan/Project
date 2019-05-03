848c928cbbf1b53663cc63fd7afd9ca3b6f9b655
shchauhan <shipra.chauhan@soprastera.com>
2019-05-03 17:05:25 +0530

    Component Scan
    Component Scan

---
+++ spring_step_1/src/main/java/com/example/spring_step_1/SpringStep1BasicComponentScanApplication.java
@@ -0,0 +1,37 @@
+
+//The package is not a part of component scan path
+package com.example.spring_step_1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.spring_step_1.basics.componentscan.ComponentDAO;
+
+@SpringBootApplication
+@ComponentScan("com.spring_step_1")
+public class SpringStep1BasicComponentScanApplication {
+
+	// What are the beans?
+	// What are the dependencies of a bean?
+	// Where to search for beans? => No need
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringStep1BasicComponentScanApplication.class);
+
+	public static void main(String[] args) {
+
+		
+				// BinarySearchImpl binarySearch =
+				// new BinarySearchImpl(new QuickSortAlgorithm());
+				// Application Context
+				ApplicationContext applicationContext = SpringApplication.run(SpringStep1BasicApplication.class, args);
+				ComponentDAO compDAO = applicationContext.getBean(ComponentDAO.class);
+				LOGGER.info("{}", compDAO);
+				LOGGER.info("{}", compDAO.getJdbcConnection());
+				
+	}
+
+}
---
+++ spring_step_1/src/main/java/com/spring_step_1/basics/componentscan/ComponentDAO.java
@@ -0,0 +1,24 @@
+package com.spring_step_1.basics.componentscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ComponentDAO {
+
+	@Autowired
+	ComponentJDBCConnection jdbcConnection;
+
+	public ComponentJDBCConnection getJdbcConnection() {
+		return jdbcConnection;
+	}
+
+	public void setJdbcConnection(ComponentJDBCConnection jdbcConnection) {
+		this.jdbcConnection = jdbcConnection;
+	}
+}

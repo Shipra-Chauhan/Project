3a0b5b9339b5275afd4a1f1ed85da0e818de2dbb
shchauhan <shipra.chauhan@soprastera.com>
2019-05-06 18:10:05 +0530

    Spring Level 2 Step 19 (Incomplete)

--- spring_step_1/src/main/java/com/example/spring_step_1/SpringStep1BasicApplication.java
+++ spring_step_1/src/main/java/com/example/spring_step_1/SpringStep1BasicApplication.java
@@ -3,10 +3,15 @@ package com.example.spring_step_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.example.spring_step_1.basics.BinarySearchImpl;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+//@SpringBootApplication
 public class SpringStep1BasicApplication {
 
 	// What are the beans?
@@ -19,7 +24,8 @@ public class SpringStep1BasicApplication {
 		// new BinarySearchImpl(new QuickSortAlgorithm());
 		// Application Context
 		ApplicationContext applicationContext = 
-				SpringApplication.run(SpringStep1BasicScopeApplication.class, args);
+				new AnnotationConfigApplicationContext(SpringStep1BasicApplication.class);
+				//SpringApplication.run(SpringStep1BasicScopeApplication.class, args);
 		BinarySearchImpl binarySearch = 
 				applicationContext.getBean(BinarySearchImpl.class);
 		BinarySearchImpl binarySearch1 = 
--- spring_step_1/src/main/java/com/example/spring_step_1/basics/BinarySearchImpl.java
+++ spring_step_1/src/main/java/com/example/spring_step_1/basics/BinarySearchImpl.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@ComponentScan    // or @ComponentScan("com.example.spring_step_1.basics")
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 

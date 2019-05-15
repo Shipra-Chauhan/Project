cd52c66a1189c8b6f17b039e57fa6d3fe0bc99c1
shchauhan <shipra.chauhan@soprastera.com>
2019-05-15 12:43:01 +0530

    Spring Boot - Baledung - First Project

---
+++ first_basic/src/main/java/com/spring_boot/example/first_basic/FirstBasicApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.example.first_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBasicApplication.class, args);
+	}
+
+}

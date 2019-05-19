bd859586fd22f258c3c471d3f563db62c16f2e6f
Shipra-Chauhan <35420333+Shipra-Chauhan@users.noreply.github.com>
2019-05-19 13:20:57 +0530

    changes

--- first_basic/src/main/java/com/spring_boot/example/first_basic/FirstBasicApplication.java
+++ first_basic/src/main/java/com/spring_boot/example/first_basic/FirstBasicApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ServletComponentScan
@@ -18,4 +19,8 @@ public class FirstBasicApplication {
 		SpringApplication.run(FirstBasicApplication.class, args);
 	}
 
+	/*@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}*/
 }
